894d2fcf73a2e11366374cb4b7877a999ae41b5c
SeungJu Lee <me@sjlee.me>
2016-02-28 11:49:00 -0800

    Added forecast.io

--- fetch_forecastio.py
+++ fetch_forecastio.py
@@ -2,13 +2,8 @@ import forecastio
 from secrets import FORECAST_IO_API_KEY
 import arrow
 
-
-lat, lng = 37.7576171, -122.5776844
-
-# time =
-forecast = forecastio.load_forecast(FORECAST_IO_API_KEY, lat, lng, time=None, units="us" )
-
-byHour = forecast.hourly()
-print (byHour.summary)
-print (byHour.icon)
-# print (byHour.precipProbability)
+def get_preci_prob_by_loc_time(lat, lng, time):
+    time = time.datetime
+    forecast = forecastio.load_forecast(FORECAST_IO_API_KEY, lat, lng, time, units="us")
+    byHour = forecast.currently()
+    return byHour.precipProbability
